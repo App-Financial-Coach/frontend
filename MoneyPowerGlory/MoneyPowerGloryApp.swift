dbd6815c734ee3d1bc669c740acece47af5ec8d8
Ayca Gökdağ <ayca.gokdag@ykteknoloji.com.tr>
2023-11-11 14:59:42 +0300

    Initial Commit

---
+++ MoneyPowerGlory/MoneyPowerGloryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyPowerGloryApp.swift
+//  MoneyPowerGlory
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyPowerGloryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
